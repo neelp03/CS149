8694b8bb94237310a066bf349d80ba4e3e80e27f
Megan Ju <megan.ju@sjsu.edu>
2023-02-18 00:12:18 +0000

    5.21 done

--- WS3/5_21.c
+++ WS3/5_21.c
@@ -7,35 +7,51 @@
  **/
  
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_WORD_LENGTH 10
 
 /**
  * This function takes in a list of words and prints out
  * the words that contain a certain character
  * Assumption: At least one word in the list will contain 
  * the given character, the list will always contain less 
- * than 20 words
+ * than 20 words, each word will contain less than 10 characters
+ * and no spaces
  * Input parameters: none
  * Returns: an integer
  **/
  int main(void){
  
  	int totalWords;
+ 	int count;
  	char searchChar;
  	char list[19][10];
- 	char temp;
+ 	char temp[MAX_WORD_LENGTH];
  	
- 	scanf("%d", totalWords);
+ 	scanf("%d", &totalWords);
  	
  	for(int i = 0; i <= totalWords; i++){
+ 		
  		if(i == totalWords){
- 			scanf("%c", searchChar);
+ 			scanf(" %c", &searchChar);
  			break;
  		}
  		
- 		
- 		
- 		list[i] = 
+ 		scanf("%s", temp);
+ 		strcpy(list[i], temp);	
  	}
+ 	
+
+ 	
+ 	for(int x = 0; x < totalWords; x++){
+ 		if(strchr(list[x], searchChar) != NULL){
+ 			printf("%s,", list[x]);
+ 		}
+ 	}
+ 	
+ 	printf("\n");
+ 	return 0;
  	
  	
  

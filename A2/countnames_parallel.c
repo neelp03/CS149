69ce6e91a44f6b7333e51a3c764ebef658385fe4
Neel P <np.patelneel@gmail.com>
2023-03-02 11:14:52 -0800

    started assignment 2

---
+++ A2/countnames_parallel.c
@@ -0,0 +1,8 @@
+/**
+ * Description: This utility counts the number of times a name appears in a set of files
+ * Author name: Neel Patel; Megan Ju
+ * Author email: neel.patel01@sjsu.edu; megan.ju@sjsu.edu
+ * Last modified date: March 2, 2023
+ * Creation date: March 2, 2023
+ **/
+

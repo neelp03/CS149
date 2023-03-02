81c8a07ca743956f8b9f1833e4b3e1b3b2be3ce4
Megan Ju <megan.ju@sjsu.edu>
2023-03-02 23:49:33 +0000

    ws5.c done

---
+++ WS5/ws5.c
@@ -0,0 +1,30 @@
+/**
+ * Description: Coding practice
+ * Author names: Megan Ju, Neel Patel
+ * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
+ * Last modified date: March 2, 2023
+ * Creation date: March 2, 2023
+ **/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * This function converts track laps to miles
+ * Assumption: none
+ * Input parameters: none
+ * Returns: an integer
+ **/
+int main(void) {
+
+    fork();
+    fork();
+    printf("hello world from PID %ld!\n", (long)getpid());
+    exit(0);
+}
+
+
+
+

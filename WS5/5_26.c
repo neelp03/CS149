9153f94f323ae6337f64f6359ac0d932d4af2c32
Megan Ju <megan.ju@sjsu.edu>
2023-03-04 03:40:42 +0000

    5.26 done

--- WS5/5_26.c
+++ WS5/5_26.c
@@ -9,16 +9,41 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_CHARS 50
+
 
 /**
  * This function counts the number of characters from 
  * input
- * Assumption: Input will not exceed 50 chars
+ * Assumption: Input will not exceed 50 characters
  * Input parameters: none
  * Returns: an integer
  **/
 int main(void) {
 
+	char input[MAX_CHARS];
+	int index = 0;
+	int count = 0;
+	
+	if(fgets(input, MAX_CHARS, stdin) == NULL) {
+		fprintf(stderr, "Error: Input Error\n");
+		return 1;
+	}
+	
+	while(input[index]){
+		if((input[index] != ' ')&&(input[index] != '.')&&
+		(input[index] != '!')&&(input[index] != ',')){
+			printf("This is my value: %c\n", input[index]);
+			count++;
+		}
+		index++;
+	}
+	
+	//subtracting one from count to account for null termination
+	count--;
+	
+	printf("%d\n", count);
+	return 0;
     
 }
 

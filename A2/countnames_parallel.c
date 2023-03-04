3491ce66a910ec76e7020f07cdd84313546689c0
Neel Patel <np.patelneel@gmail.com>
2023-03-03 22:35:16 -0800

    attempting to not count spaces as names

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -1,153 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_NAMES 1000
+#define MAX_FILES 100
+#define MAX_NAMES 100
 #define MAX_NAME_LEN 50
-#define BUF_SIZE 100
+#define MAX_LINE_LEN 200
 
 int main(int argc, char *argv[])
 {
-    // Declare names and counts arrays to hold names and counts
-    char names[MAX_NAMES][MAX_NAME_LEN];
-    int counts[MAX_NAMES] = {0};
+    // Check that at least one file is provided
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <file1> [<file2> ...]\n", argv[0]);
+        exit(1);
+    }
+
+    // Initialize variables
+    int num_files = argc - 1;
+    int pid[MAX_FILES];
+    int pipes[MAX_FILES][2];
+    char *names[MAX_NAMES];
+    int name_count[MAX_NAMES] = {0};
     int num_names = 0;
+    int name_idx[MAX_FILES];
 
-    // Iterate over files and count names
-    for (int i = 1; i < argc; i++)
+    // Loop through all files and create child processes to count names
+    for (int i = 0; i < num_files; i++)
     {
-        FILE *file = fopen(argv[i], "r");
-        if (!file)
+        // Create pipe for communication between parent and child processes
+        if (pipe(pipes[i]) == -1)
         {
-            perror(argv[i]);
-            continue;
-        }
-
-        char buf[BUF_SIZE];
-        while (fgets(buf, BUF_SIZE, file))
-        {
-            // Trim trailing newline
-            buf[strcspn(buf, "\n")] = '\0';
-
-            // Check if name already exists in names array
-            int found = 0;
-            for (int j = 0; j < num_names; j++)
-            {
-                if (strcmp(buf, names[j]) == 0)
-                {
-                    found = 1;
-                    counts[j]++;
-                    break;
-                }
-            }
-
-            // If name doesn't exist in names array, add it
-            if (!found)
-            {
-                strcpy(names[num_names], buf);
-                counts[num_names]++;
-                num_names++;
-            }
-        }
-
-        fclose(file);
-    }
-
-    // Create pipes for communicating counts from child processes to parent process
-    int fd[num_names][2];
-    for (int i = 0; i < num_names; i++)
-    {
-        if (pipe(fd[i]) == -1)
-        {
-            perror("pipe");
+            perror("Error creating pipe");
             exit(1);
         }
-    }
 
-    // Fork child processes to count names
-    for (int i = 0; i < num_names; i++)
-    {
-        pid_t pid = fork();
-        if (pid == -1)
+        // Fork child process
+        pid[i] = fork();
+        if (pid[i] == -1)
         {
-            perror("fork");
+            perror("Error forking process");
             exit(1);
         }
-        else if (pid == 0)
-        { // Child process
-            // Close unused write end of pipe
-            close(fd[i][1]);
+        else if (pid[i] == 0)
+        {
+            // Child process
+            close(pipes[i][0]); // Close read end of pipe
 
-            // Read from file and count name
-            int count = 0;
-            FILE *file = fopen(argv[1], "r");
-            if (!file)
+            // Open file for reading
+            FILE *fp = fopen(argv[i + 1], "r");
+            if (fp == NULL)
             {
-                perror(argv[1]);
+                perror("Error opening file");
                 exit(1);
             }
 
-            char buf[BUF_SIZE];
-            while (fgets(buf, BUF_SIZE, file))
+            // Read file line by line and count names
+            char line[MAX_LINE_LEN];
+            while (fgets(line, MAX_LINE_LEN, fp) != NULL)
             {
-                // Trim trailing newline
-                buf[strcspn(buf, "\n")] = '\0';
-
-                if (strcmp(buf, names[i]) == 0)
+                // Tokenize line by spaces
+                char *token = strtok(line, " \t\n\r");
+                while (token != NULL)
                 {
-                    count++;
+                    // Check if token is a new name
+                    int name_found = 0;
+                    for (int j = 0; j < num_names; j++)
+                    {
+                        if (strcmp(token, names[j]) == 0)
+                        {
+                            name_found = 1;
+                            name_idx[i] = j;
+                            break;
+                        }
+                    }
+                    if (!name_found)
+                    {
+                        // Add new name to list
+                        names[num_names] = strdup(token);
+                        name_count[num_names] = 0;
+                        name_idx[i] = num_names;
+                        num_names++;
+                    }
+
+                    // Increment count for name
+                    name_count[name_idx[i]]++;
+
+                    // Get next token
+                    token = strtok(NULL, " \t\n\r");
                 }
             }
 
-            fclose(file);
-
-            // Write count to pipe
-            write(fd[i][0], &count, sizeof(int));
-
-            // Close read end of pipe
-            close(fd[i][0]);
-
-            // Exit child process
+            // Write count to pipe and exit
+            if (write(pipes[i][1], &name_count[name_idx[i]], sizeof(int)) == -1)
+            {
+                perror("Error writing count to pipe");
+                exit(1);
+            }
+            close(pipes[i][1]); // Close write end of pipe
             exit(0);
         }
         else
-        { // Parent process
-            // Close unused read end of pipe
-            close(fd[i][0]);
+        {
+            // Parent process
+            close(pipes[i][1]); // Close write end of pipe
         }
     }
 
-    // Wait for child processes to finish and read counts from pipes
     // Loop through child processes and read counts from pipes
     for (int i = 0; i < num_files; i++)
     {
+        // Wait for child process to finish
         int status;
-        if (waitpid(pid[i], &status, 0) == -1)
+        waitpid(pid[i], &status, 0);
+
+        // Read count from pipe
+        int count;
+        if (read(pipes[i][0], &count, sizeof(int)) == -1)
         {
-            fprintf(stderr, "Error waiting for child process %d\n", pid[i]);
-            continue;
+            perror("Error reading count from pipe");
+            exit(1);
         }
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
-        {
-            int count;
-            if (read(pipes[i][0], &count, sizeof(count)) == -1)
-            {
-                fprintf(stderr, "Error reading count from pipe %d\n", i);
-                continue;
-            }
-            name_count[name_idx[i]] += count;
-        }
-        else
-        {
-            fprintf(stderr, "Child process %d exited with error status %d\n", pid[i], WEXITSTATUS(status));
-        }
-        close(pipes[i][0]);
-    }
-    // Print final counts for each name
-    for (int i = 0; i < num_names; i++)
-    {
-        printf("%s:%d\n", names[i], name_count[i]);
+        close(pipes[i][0]); // Close read end of pipe
+
+        // Print count for name
+        printf("%s: %d\n", names[name_idx[i]], count);
     }
     return 0;
 }

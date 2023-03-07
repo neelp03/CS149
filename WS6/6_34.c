1dd60fd7f1b50fa1dae32610ed21f5f9655a91fd
Megan Ju <megan.ju@sjsu.edu>
2023-03-07 22:45:12 +0000

    started 6.34 lab

---
+++ WS6/6_34.c
@@ -0,0 +1,7 @@
+/**
+ * Description: Exercise 6.34 Multiples of ten in an array from Zybooks
+ * Author names: Megan Ju, Neel Patel
+ * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
+ * Last modified date: March 7, 2023
+ * Creation date: March 7, 2023
+ **/

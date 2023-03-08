bcaeaf02792f7c46f1e1c85c3b5d6b06dd54b80b
Megan Ju <megan.ju@sjsu.edu>
2023-03-08 05:24:43 +0000

    making this now so don't procrastinate

---
+++ proc_manager.c
@@ -0,0 +1,14 @@
+/**
+  * Description: This utility executes multiple commands
+  * Author name: Neel Patel; Megan Ju
+  * Author email: neel.patel01@sjsu.edu; megan.ju@sjsu.edu
+  * Last modified date: March 7, 2023
+  * Creation date: March 7, 2023
+  **/
+  
+ #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+  
+  int main(){
+  }

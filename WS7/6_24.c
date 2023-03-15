b8ea5ca4aad9f76680bc3599bc7e8923b05ba1f7
Megan Ju <megan.ju@sjsu.edu>
2023-03-15 18:22:13 +0000

    started 6.24 lab

---
+++ WS7/6_24.c
@@ -0,0 +1,14 @@
+/**
+ * Description: Exercise 6.24 Swapping variables from Zybooks
+ * Author names: Megan Ju, Neel Patel
+ * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
+ * Last modified date: March 15, 2023
+ * Creation date: March 15, 2023
+ **/
+ 
+ void SwapValues(int* userVal1, int* userVal2, int* userVal3, int* userVal4){
+ }
+ 
+ int main(void){
+ 
+ }

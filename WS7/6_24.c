80806386ad24c6c50361b59573639b138e9adc89
Megan Ju <megan.ju@sjsu.edu>
2023-03-16 06:10:46 +0000

    6.24 done, lseek uploaded

--- WS7/6_24.c
+++ WS7/6_24.c
@@ -6,9 +6,43 @@
  * Creation date: March 15, 2023
  **/
  
+ #include <stdio.h>
+ 
+  /**
+ * This function swaps the first with the second and the third with the fourth values
+ * Assumption: None
+ * Input parameters: '*userVal1' - first value
+ 		     '*userVal2' - second value
+ 		     '*userVal3' - third value
+  		     '*userVal4' - fourth value
+ * Returns: none
+ **/
  void SwapValues(int* userVal1, int* userVal2, int* userVal3, int* userVal4){
+ 
+ 	int temp;
+ 	temp = *userVal2;
+ 	*userVal2 = *userVal1;
+ 	*userVal1 = temp;
+ 	temp = *userVal4;
+ 	*userVal4 = *userVal3;
+ 	*userVal3 = temp;
  }
  
- int main(void){
  
+ /**
+ * This function calls the SwapValues function
+ * Assumption: Reads only four integers
+ * Input parameters: none
+ * Returns: an integer
+ **/
+ int main(void){
+ 	int first, second, third, fourth;
+ 	
+ 	scanf("%d %d %d %d", &first, &second, &third, &fourth);
+ 	
+ 	SwapValues(&first, &second, &third, &fourth);
+ 	
+ 	printf("%d %d %d %d\n", first, second, third, fourth);
+ 	
+ 	return 0;
  }

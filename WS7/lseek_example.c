665d9f8221ac658023ca56f2090f47a9d2654028
Megan Ju <megan.ju@sjsu.edu>
2023-03-19 01:06:00 +0000

    lseek done

--- WS7/lseek_example.c
+++ WS7/lseek_example.c
@@ -13,27 +13,24 @@ void func(char arr[], int n)
 	int f_write = open("end.txt", O_WRONLY | O_CREAT, 0777);
 
 	int count = 0;
+	//read a single bite of the array
 	while (read(f_read, arr, 1))
 	{
+	
 		// to write the 1st byte of the input file in
-		// the output file
-		if (count < n)
-		{
+        	// the output file
+		if(count < n){
 			// SEEK_CUR specifies that
 			// the offset provided is relative to the
 			// current file position
-			lseek (f_read, n, SEEK_CUR);
-			write (f_write, arr, 1);
+			lseek(f_read, n, SEEK_CUR);
+			write(f_write, arr, 1);
 			count = n;
 		}
 		
-			// After the nth byte (now taking the alternate
-			// nth byte)
-		else
-		{
-			count = (2*n);
+		else {
 			lseek(f_read, count, SEEK_CUR);
-			write(f_write, arr, 1);
+			write(f_write, arr, 1);	
 		}
 	}
 	close(f_write);
@@ -44,7 +41,7 @@ int main()
 {
 	char arr[100];
 	int n;
-	n = 5;
+	n = 2;
 	
 	// Calling for the function
 	func(arr, n);

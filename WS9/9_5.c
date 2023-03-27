0df4f3c8bcef519c3850e79138c7040901c5eacc
Megan Ju <megan.ju@sjsu.edu>
2023-03-27 23:13:02 +0000

    starting wkst 9

---
+++ WS9/9_5.c
@@ -0,0 +1,17 @@
+/**
+ * Description: Exercise 9.5 - Parsing strings from Zybooks
+ * Author names: Megan Ju, Neel Patel
+ * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
+ * Last modified date: March 27, 2023
+ * Creation date: March 27, 2023
+ **/
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+
+	printf("Enter input string:\n);
+
+	return 0;
+}

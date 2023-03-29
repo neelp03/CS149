fb158ceea0ccdb2c08a08f8dbb229e0f2df5f313
Megan Ju <megan.ju@sjsu.edu>
2023-03-29 04:29:57 +0000

    updated with getline, quitting is still broken

--- WS9/9_5.c
+++ WS9/9_5.c
@@ -1,16 +1,7 @@
-/**
- * Description: Exercise 9.5 - Parsing strings from Zybooks
- * Author names: Megan Ju, Neel Patel
- * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
- * Last modified date: March 28, 2023
- * Creation date: March 27, 2023
- **/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-#define MAX_LENGTH 50
 
 /**
  * Parses string to output the two strings separated by a comma
@@ -20,39 +11,45 @@
  */
 int main() {
 
-	char input[MAX_LENGTH + 1];
-	char ch[2] = ",";
-	char *first, *second, *token;
-	char stop[2] = "q";
-	bool done = false;
+    char buffer[32];
+    char *b = buffer;
+    char *first, *second, *token;
+    size_t bufsize = 32;
+    char ch[2] = ",";
+    char quit[3] = "q1";
+    int loop = 1;
 
+while(loop){
+
+    printf("Enter input string:\n");
+    getline(&b, &bufsize, stdin);
+
+    if(strcmp(buffer, quit) == 0){
+    	printf("not working\n");
+    	loop = 0;
+    }
+    
+    else {
+    
+    	if(strchr(buffer, (int)ch[0]) == NULL){
+    		fprintf(stderr, "Error: No comma in string.\n");
+    	}
 	
-	while(!done){
-	
-		printf("Enter input string:\n");
-		fgets(input, MAX_LENGTH+1, stdin);
-	
-		if(strcmp(input, stop) == 0){
-			done = true;
-			break;
-		}
-	
-		if(strchr(input, (int)ch[0]) == NULL){
-			fprintf(stderr, "Error: No comma in string.\n");
-		}
-		else{
-			token = strtok(input, ch);
-			first = token;
-			if(token!=NULL){
-				token = strtok(NULL, ch);
-				second = token;
-			}
+    	else {
+    		token = strtok(buffer, ch);
+    		first = token;
+    		if(token!=NULL){
+    			token = strtok(NULL, ch);
+    			second = token;
+    		}
 			
-			printf("First word: %s\n", first);
-			printf("Second word: %s\n", second); 
-		}
-		
+		printf("First word: %s\n", first);
+		printf("Second word: %s\n", second); 
 	}
 
-	return 0;
+    }
+    
+}
+
+    return(0);
 }

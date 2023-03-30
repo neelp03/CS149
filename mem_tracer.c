17a1f0117d6a906088a1fd973198415b7ed7ebda
Megan <67922354+Megan-J@users.noreply.github.com>
2023-03-29 23:03:57 -0700

    Delete mem_tracer.c

--- mem_tracer.c
+++
@@ -1,7 +0,0 @@
-/**
- * Description: Assignment 4
- * Author names: Megan Ju, Neel Patel
- * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
- * Last modified date: March 29, 2023
- * Creation date: March 29, 2023
- **/

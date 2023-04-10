806dd25610fba51d22064da639249787c267d577
Neel P <np.patelneel@gmail.com>
2023-04-10 01:57:07 -0700

    WHY IS IT NOT WORKING Y_Y

--- A4/mem_tracer.c
+++ A4/mem_tracer.c
@@ -1,125 +1,226 @@
-/**
- * Description: Assignment 4
- * Author names: Megan Ju, Neel Patel
- * Author emails: megan.ju@sjsu.edu, neel.patel@sjsu.edu
- * Last modified date: April 5, 2023
- * Creation date: March 29, 2023
- **/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-
-/**
-*/ TRACE_NODE_STRUCT is a linked list of
-// pointers to function identifiers
-// TRACE_TOP is the head of the list is the top of the stack
-
-struct TRACE_NODE_STRUCT {
-char functionid; // ptr to function identifier (a function name)
-struct TRACE_NODE_STRUCT next; // ptr to next frama
-};
-typedef struct TRACE_NODE_STRUCT TRACE_NODE;
-static TRACE_NODE TRACE_TOP = NULL; // ptr to the top of the stack
-
-/* --------------------------------/
-/ function PUSH_TRACE /
-
-The purpose of this stack is to trace the sequence of function calls,
-just like the stack in your computer would do.
-The "global" string denotes the start of the function call trace.
-The char *p parameter is the name of the new function that is added to the call trace.
-See the examples of calling PUSH_TRACE and POP_TRACE below
-in the main, make_extend_array, add_column functions.
-*/
-void PUSH_TRACE(char p) // push p on the stack
-{
-TRACE_NODE tnode;
-static char glob[]="global";
-if (TRACE_TOP==NULL) {
-// initialize the stack with "global" identifier
-TRACE_TOP=(TRACE_NODE*) malloc(sizeof(TRACE_NODE));
-
-// no recovery needed if allocation failed, this is only
-// used in debugging, not in production
-if (TRACE_TOP==NULL) {
-  printf("PUSH_TRACE: memory allocation error\n");
-  exit(1);
-}
-
-TRACE_TOP->functionid = glob;
-TRACE_TOP->next=NULL;
-}//if
-
-// create the node for p
-tnode = (TRACE_NODE*) malloc(sizeof(TRACE_NODE));
-
-// no recovery needed if allocation failed, this is only
-// used in debugging, not in production
-if (tnode==NULL) {
-printf("PUSH_TRACE: memory allocation error\n");
-exit(1);
-}//if
-
-tnode->functionid=p;
-tnode->next = TRACE_TOP; // insert fnode as the first in the list
-TRACE_TOP=tnode; // point TRACE_TOP to the first node
-
-}
-//end PUSH_TRACE/
-
-/* --------------------------------/
-/ function POP_TRACE /
-/ Pop a function call from the stack /
-*/
-void POP_TRACE() // remove the op of the stack
-{
-TRACE_NODE tnode;
-tnode = TRACE_TOP;
-TRACE_TOP = tnode->next;
-free(tnode);
-
-}
-// end POP_TRACE
+#include <stdio.h>   // for printf, FILE, fopen, fclose, fprintf
+#include <stdlib.h>  // for malloc, free, exit
+#include <string.h>  // for strcmp, strlen, strncpy
+#include <fcntl.h>   // for open, O_CREAT, O_WRONLY, O_TRUNC
+#include <unistd.h>  // for write
+#include <sys/stat.h> // for S_IRUSR, S_IWUSR
 
 
-/* ---------------------------------------------- */
-/* function PRINT_TRACE prints out the sequence of function calls that are on the stack at this instance */
-/* For example, it returns a string that looks like: global:funcA:funcB:funcC. */
-/* Printing the function call sequence the other way around is also ok: funcC:funcB:funcA:global */
+// Node structure for linked list
+typedef struct node {
+    char* line;
+    int index;
+    struct node* next;
+    char* function;
+} node_t;
 
-char* PRINT_TRACE()
-{
-  int depth = 50; // A max of 50 levels in the stack will be combined in a string for printing out.
-  int i, length, j;
-  TRACE_NODE* tnode;
-  static char buf[100];
+// Stack structure for function calls
+typedef struct stack {
+    int top;
+    char** function;
+    int size;
+} stack_t;
 
-  if (TRACE_TOP == NULL) {     // stack not initialized yet, so we are
-    strcpy(buf, "global");     // still in the `global' area
-    return buf;
-  }
+// Function prototypes
+char** make_extend_array(char** array, int size, int increment, int* count);
+node_t* add_node(node_t* head, char* line, int index);
+void print_nodes(node_t* head);
+void push(stack_t* stack, char* function);
+char* pop(stack_t* stack);
+void mem_alloc(char* function, int size);
+void mem_realloc(char* function, void* ptr, int size);
+void mem_free(char* function, void* ptr);
 
-  /* Peek at the top depth (50) entries on the stack, but do not
-     go over 100 chars and do not go over the bottom of the stack */
-  sprintf(buf, "%s", TRACE_TOP->functionid);
-  length = strlen(buf);        // length of the string so far
-  for (i = 1, tnode = TRACE_TOP->next; tnode != NULL && i < depth; i++, tnode = tnode->next) {
-    j = strlen(tnode->functionid);    // length of what we want to add
-    if (length + j + 1 < 100) {       // total length is ok
-      sprintf(buf + length, ":%s", tnode->functionid);
-      length += j + 1;
-    } else {                          // it would be too long
-      break;
+int main() {
+    // Redirect stdout to memtrace.out
+    int fd = open("memtrace.out", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    dup2(fd, STDOUT_FILENO);
+
+    char** array = NULL;
+    int size = 10;
+    int increment = 10;
+    int count = 0;
+
+    // Initialize stack for memory tracing
+    stack_t stack;
+    stack.top = -1;
+    stack.function = malloc(sizeof(char*) * 100);
+
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    node_t* head = NULL;
+    int index = 0;
+
+    // Read lines from stdin
+    while ((read = getline(&line, &len, stdin)) != -1) {
+        // Remove newline character at end of line
+        if (line[read - 1] == '\n') {
+            line[read - 1] = '\0';
+        }
+
+        // Store line in array
+        if (count >= size) {
+            array = make_extend_array(array, size, increment, &count);
+            push(&stack, "make_extend_array");
+        }
+        array[count] = malloc(strlen(line) + 1);
+        push(&stack, "malloc");
+        strcpy(array[count], line);
+        count++;
+
+        // Store line in linked list
+        head = add_node(head, line, index);
+        push(&stack, "add_node");
+        index++;
     }
-  }
 
-  // Print info about memory usage
-  printf("File %s, line %d, function %s ", __FILE__, __LINE__, __func__);
-  printf("allocated memory segment at address %p to size %d\n", buf, length);
-  printf("Current call trace: %s\n", buf);
+    // Print content of linked list
+    print_nodes(head);
+    push(&stack, "print_nodes");
 
-  return buf;
-} /* end PRINT_TRACE */
+    // Free memory allocated for linked list
+    node_t* current = head;
+    while (current != NULL) {
+        node_t* next = current->next;
+        free(current->line);
+        push(&stack, "free");
+        free(current);
+        push(&stack, "free");
+        current = next;
+    }
+
+    // Free memory allocated for array
+    for (int i = 0; i < count; i++) {
+        free(array[i]);
+        push(&stack, "free");
+    }
+    free(array);
+    push(&stack, "free");
+
+    // Free memory allocated for stack
+    free(stack.function);
+    push(&stack, "free");
+
+    return 0;
+}
+
+// Function to allocate and reallocate memory for array
+char** make_extend_array(char** array, int size, int increment, int* count) {
+    // Print memory allocation message
+    mem_alloc("make_extend_array", sizeof(char*) * (size + increment));
+
+    // Reallocate memory for array
+    array = realloc(array, sizeof(char*) * (size + increment));
+
+    // Print memory reallocation message
+	mem_realloc("make_extend_array", (void**) &array, sizeof(char*) * (size + increment));
+
+	// Check if reallocation was successful
+	if (array == NULL) {
+		printf("Error: could not reallocate memory for array\n");
+		exit(1);
+	}
+
+	// Increment count by the number of new elements added to array
+	*count += increment;
+
+	// Return the updated array
+	return array;
+}
+
+node_t* add_node(node_t* head, char* line, int index) {
+    // Allocate memory for new node
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+
+    // Initialize new node
+    new_node->line = line;
+    new_node->index = index;
+    new_node->next = NULL;
+
+    // If list is empty, new node is head
+    if (head == NULL) {
+        head = new_node;
+    }
+    // Otherwise, add new node to end of list
+    else {
+        node_t* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+
+    // Return updated head
+    return head;
+}
+
+void print_nodes(node_t* head) {
+    node_t* current = head;
+
+    while (current != NULL) {
+        printf("%p\n", current->function);
+        current = current->next;
+    }
+}
+
+void push(stack_t* stack, char* function) {
+    // Create new node for function
+    node_t* new_node = (node_t*) malloc(sizeof(node_t));
+    new_node->function = function;
+    new_node->next = NULL;
+
+    // Add new node to top of stack
+    new_node->next = (node_t*)stack->top;
+	stack->top = (void*)new_node;
+
+    stack->size++;
+}
+
+// Function to remove the top element from the stack and return it
+void* pop(stack_t* stack) {
+    // Check if stack is empty
+    if (stack->top != NULL) {
+        printf("Error: stack is empty\n");
+        return NULL;
+    }
+
+    // Store the top element and remove it from the stack
+    void (*function)() = ((node_t*)stack->top)->function;
+
+    int* temp = (int*)stack->top;
+    stack->top = (node_t *)stack->top->next;
+    free(temp);
+
+    return function;
+}
+
+// Function to print memory allocation message
+void mem_alloc(char* function, int size) {
+    printf("Memory allocated in function %s, size = %d\n", function, size);
+}
+
+void mem_realloc(char* function, void* ptr, int size) {
+    // Print memory reallocation message
+    printf("file-\"%s\", function=%s, segment reallocated to address %p\n", __FILE__, function, ptr);
+    
+    // Reallocate memory
+    ptr = realloc(ptr, size);
+    
+    // Check if reallocation failed
+    if (ptr == NULL) {
+        fprintf(stderr, "Memory reallocation failed in function %s\n", function);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void mem_free(char* function, void* ptr) {
+    // Print memory deallocation message
+    printf("file-\"%s\", function=%s, segment freed at address %p\n", __FILE__, function, ptr);
+    
+    // Free memory
+    free(ptr);
+}
 
 
